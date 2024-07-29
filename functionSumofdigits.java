5725ef5564bd503fbf477b018111b6bd5fcc9b2f
kavin108 <145475464+kavin108@users.noreply.github.com>
2024-07-29 11:32:36 +0530

    FUNCTION

---
+++ functionSumofdigits.java
@@ -0,0 +1,19 @@
+package practice;
+class giri{
+	public int sum(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+}
+public class functionSumofdigits {
+public static void main(String[] args) {
+	giri g=new giri();
+	int n=g.sum(5,6,7);
+	System.out.println(n);
+			
+	
+	
+
+	}
+
+}
